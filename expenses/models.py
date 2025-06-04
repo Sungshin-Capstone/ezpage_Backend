4cc0c28d22842df37167beda77b1cb584302f37f
kimyejun030626 <eunjun6570@gmail.com>
2025-06-05 03:49:18 +0900

    지불가이드 결과 반영_11

--- expenses/models.py
+++ expenses/models.py
@@ -15,8 +15,8 @@ class Expense(models.Model):
 
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='expenses')
     is_scan_result = models.BooleanField(default=False, verbose_name="스캔 결과 여부")
-    trip = models.ForeignKey('expenses.Trip', on_delete=models.CASCADE)
-    currency = models.CharField(max_length=10)  
+    currency = models.CharField(max_length=10, null=True, blank=True)
+    trip = models.ForeignKey('Trip', on_delete=models.CASCADE, null=True, blank=True)
     amount = models.DecimalField(max_digits=12, decimal_places=2, verbose_name="지출 금액")
     currency_code = models.CharField(max_length=10, verbose_name="통화 코드")
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='OTHER', verbose_name="카테고리")
